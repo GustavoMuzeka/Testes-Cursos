e72a09fa36920f5abf35613fee70eedae236246d
Gustavo Muzeka <muzeka89@gmail.com>
2023-04-20 14:23:34 -0300

    testes

---
+++ Exercicios-logicaprogramacao/ex01.js
@@ -0,0 +1,7 @@
+
+function calcular(){
+    let n1 = Number(document.getElementById('n1').value)
+    let n2 = Number(document.getElementById('n2').value)
+
+    document.getElementById('result').innerHTML = `Testando, numero escolhido foi ${n1} e ${n2}`
+}
