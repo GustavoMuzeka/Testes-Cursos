abea416d19cb815f49aaa983c8851a8b1501d8a7
Gustavo Muzeka <muzeka89@gmail.com>
2023-04-24 16:48:50 -0300

    teste

--- Typescript/dist/app.js
+++
@@ -1,8 +0,0 @@
-width = prompt('Digite a largura da imagem')
-height = prompt('Digite a altura da imagem')
-
-const image = function imagem(width, height){
-    if (width > height){
-        return console.log(`A imagem está no modo paisagem`)
-    }console.log('Não e')
-}
